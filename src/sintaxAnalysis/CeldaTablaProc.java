52d86dcf84f243a58c74d2731129c982c9ec7bbb
aalopz02 <aalopz02@gmail.com>
2020-05-26 21:04:27 -0600

    Verificacion indices lista
    Faltan agregar modificacion a indices por funciones del e insert y verificar que len se aplique a lista

---
+++ src/sintaxAnalysis/CeldaTablaProc.java
@@ -0,0 +1,55 @@
+package sintaxAnalysis;
+
+import java.util.ArrayList;
+
+public class CeldaTablaProc {
+
+    private String id;
+    private ArrayList<String> param = null;
+    private boolean dcl;
+    private ArrayList<CeldaTablaIgualdades> var = null;
+
+    CeldaTablaProc(String id, boolean dcl){
+        this.id = id;
+        this.dcl = dcl;
+        if (!dcl) {
+            var = new ArrayList<>();
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ArrayList<String> getParam() {
+        return param;
+    }
+
+    public ArrayList<CeldaTablaIgualdades> getVars() {
+        return var;
+    }
+
+    public void setParam(String paramIn) {
+        if (param == null){
+            param = new ArrayList<>();
+        } param.add(paramIn);
+    }
+
+    public void setParam(ArrayList<String> contenido, int scope){
+         CeldaTablaIgualdades cell = new CeldaTablaIgualdades(scope,contenido);
+         var.add(cell);
+    }
+
+    public boolean isDcl() {
+        return dcl;
+    }
+
+    public void setDcl(boolean dcl) {
+        this.dcl = dcl;
+    }
+
+}
